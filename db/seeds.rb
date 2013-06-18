f40b006bc765a13db6c26d947d9938dd216f28d1
Tommy Kail <tommykail@gmail.com>
2013-06-18 17:48:40 -0400

    Seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.destroy_all
+
+restaurant1 = Restaurant.create!(name: "Barbounia", teaser: "Middle Eastern cuisine at its finest.", address: "611 Park Ave.", 
+  neighborhood: "Flatiron District", price: "$$$$", summary: "A pleasant experience for lunch or dinner.", 
+  menu: "hummus, shish-ka-bobs, cous-cous, and more", timeslots: 2)
+
+restaurant2 = Restaurant.create!(name: "Turks & Frogs", teaser: "Drinks and food we can all enjoy.", address: "72 Greenwich Ave.",
+  neighborhood: "Tribeca", price: "$$$$", summary: "Delicious food and wonderful folks.", 
+  menu: "fish, chips, burgers, and beer", timeslots: 5)
+
+restaurant3 = Restaurant.create!(name: "the Odeon", teaser: "Downtown food at its best", address: "1443 W. Broadway", 
+  neighborhood: "Tribeca", price: "$$$$", summary: "A great date night", menu: "Steak, chicken, fish, fun", 
+  timeslots: 7)
+
+
